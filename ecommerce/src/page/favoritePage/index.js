a9bbcc17eaf4542f4e263cd42f2ecbceca7e423d
Ethutami <ethutami@gmail.com>
2021-07-08 09:46:44 +0800

    add navigation

---
+++ ecommerce/src/page/favoritePage/index.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+import { Text, View } from 'react-native'
+
+export default class FavoritePage extends PureComponent {
+    render() {
+        return (
+            <View>
+                <Text> favorite </Text>
+            </View>
+        )
+    }
+}
